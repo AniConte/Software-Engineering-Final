ce15801e7172720a64c61cbfa5ecb7f2066d43d2
= <=>
2025-12-23 20:49:17 +0500

    Add ManyToOne relations in EntResult

--- src/main/java/com/example/demo/entities/EntResult.java
+++ src/main/java/com/example/demo/entities/EntResult.java
@@ -6,6 +6,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -24,4 +26,12 @@ public class EntResult {
     private LocalDate date;
 
     private Integer score;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "subject_id")
+    private EntSubject subject;
 }
