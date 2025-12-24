16a31f23b37d308f7d3e6f43ca3a28ad29658032
= <=>
2025-12-24 18:19:51 +0500

    Delete unnecessary dependencies

--- src/main/java/com/example/demo/entities/EntResult.java
+++ src/main/java/com/example/demo/entities/EntResult.java
@@ -1,17 +1,12 @@
 package com.example.demo.entities;
 
 import java.time.LocalDate;
-import java.util.HashSet;
-import java.util.Set;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
